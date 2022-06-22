b39dbfe95c2f89fba9efcb92d629d296da8c1595
Jose Angel Cortez <joseangel171001@gmail.com>
2022-06-21 21:23:53 -0400

    setup laoder version 2

--- src/components/SetupLoader/SetupLoader.js
+++ src/components/SetupLoader/SetupLoader.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { Oval } from "react-loader-spinner";
 
-function SetupLoader({
-  width,
-  height,
-  strokeWidth,
-  color,
-  secondaryColor,
-  display,
-}) {
-  if (display) {
-    return (
-      <Oval
-        ariaLabel="loading-indicator"
-        height={height || 100}
-        width={width || 100}
-        strokeWidth={strokeWidth || 5}
-        color={color || "blue"}
-        secondaryColor={secondaryColor || "white"}
-      />
-    );
-  }
-}
+const SetupLoader = ({
+  width = 100,
+  height = 100,
+  strokeWidth = 5,
+  color = "blue",
+  secondaryColor = "white",
+}) => {
+  return (
+    <Oval
+      ariaLabel="loading-indicator"
+      height={height}
+      width={width}
+      strokeWidth={strokeWidth}
+      color={color}
+      secondaryColor={secondaryColor}
+    />
+  );
+};
 
 export default SetupLoader;

532a0ed9b264e715949f8a3dbdaa5c09a20dd78b
Nicolas Schleicher <nicosc41@gmail.com>
2022-06-24 15:53:41 -0300

    <feat> add  routing file and route handle to app.jsx

---
+++ src/Routing/Routers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Routers = [
+  {
+    path: '/page1',
+    element: <h1>FIRST PAGE</h1>,
+  },
+  {
+    path: '/page2',
+    element: <h1>SECOND PAGE</h1>,
+  },
+];
+
+export default Routers;

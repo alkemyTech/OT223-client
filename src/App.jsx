fc23f9b8a59fc7fca4c91a4e21b74e141652071b
u914013 <nicolas.gonzalez@baufest.com>
2022-06-24 19:06:04 -0300

    example

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './features/counter/Counter';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (

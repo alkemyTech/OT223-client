fca7dc85b315fd94ce6092ad043b2764995aecc7
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-27 17:49:13 -0300

    feat: Add Icon component and svgs

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,4 +1,32 @@
-export { default as FacebookIcon } from './FacebookSVG';
-export { default as InstagramIcon } from './InstagramSVG';
-export { default as MailIcon } from './MailSVG';
-export { default as TriangleIcon } from './TriangleSVG';
+import React from 'react';
+import { ReactComponent as Instagram } from '../../assets/images/instagram.svg';
+import { ReactComponent as Facebook } from '../../assets/images/facebook.svg';
+import { ReactComponent as Mail } from '../../assets/images/mail.svg';
+import { ReactComponent as Triangle } from '../../assets/images/triangle.svg';
+
+const types = {
+  triangle: (props) => <Triangle {...props} />,
+  instagram: (props) => <Instagram {...props} />,
+  facebook: (props) => <Facebook {...props} />,
+  mail: (props) => <Mail {...props} />,
+};
+
+const Icon = ({ type = 'triangle', size = 40, ...props }) => {
+  const px = `${Number(size) ? size : 40}px`;
+  const styles = {
+    width: px,
+    height: px,
+  };
+
+  props.style = styles;
+
+  return (
+    <>
+      {
+        types[type](props)
+      }
+    </>
+  );
+};
+
+export default Icon;

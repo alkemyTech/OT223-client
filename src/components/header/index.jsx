c3dcd63475b0d7b50caa3c62840a99daa39194cf
Alicia Pak <aliciappak@gmail.com>
2022-06-28 20:13:42 -0300

    feat: responsive added

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Link as LinkRouter } from 'react-router-dom';
 import logo from '../../assets/LOGO-SOMOS-MAS.png';
 import styles from './header.module.css';
@@ -36,14 +37,14 @@ function Header() {
     <nav className={`${styles.navbarContainer} navbar navbar-expand-lg`}>
       <div className='container-fluid'>
         <LinkRouter to='/inicio'><img src={logo} alt='logo-somos-mas' className={`${styles.logo}`} /></LinkRouter>
-        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-          <span className='navbar-toggler-icon' />
+        <button className={`${styles.menuButton}`} type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className={`${styles.toggleIcon}`}>Menu</span>
         </button>
         <div className={`${styles.navigationMenu} collapse navbar-collapse`} id='navbarSupportedContent'>
           {
             pages?.map((page) => <LinkRouter to={page.route} key={page.text} className={`${styles.menuItems}`}>{page.text}</LinkRouter>)
           }
-          <div className='d-flex'>
+          <div className={`${styles.divButtons} d-flex`}>
             <LinkRouter to='/login' className={`${styles.linksButtons}`}><button className={`${styles.logInButton}`} type='button'>Log in</button></LinkRouter>
             <LinkRouter to='/registro' className={`${styles.linksButtons}`}><button className={`${styles.registerButton}`} type='button'>Registrate</button></LinkRouter>
           </div>

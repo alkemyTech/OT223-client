5ce2729915921033010dabda36ab65c97b866b34
Alicia Pak <aliciappak@gmail.com>
2022-07-04 17:39:03 -0300

    feat: create slider

--- src/Routing/RouteHandler.js
+++ src/Routing/RouteHandler.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Routers from './Routers';
+import Slider from '../components/slider';
 
 function RouteHandler() {
   return (
     <BrowserRouter>
+      <Slider />
       <Routes>
         {Routers.map((route) => (<Route path={route.path} element={route.element} />))}
       </Routes>
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,33 +1,59 @@
 import React from 'react';
-import Carousel from 'react-grid-carousel';
+import {
+  Carousel, CarouselIndicators, CarouselItem, CarouselCaption, CarouselControl,
+} from 'reactstrap';
 import Slides from './carouselTest';
 
 function Slider() {
   return (
     <Carousel
-      cols={5}
-      rows={1}
-      gap={11}
-      responsiveLayout={[
-        {
-          breakpoint: 1200,
-          cols: 3,
-        },
-        {
-          breakpoint: 990,
-          cols: 2,
-        },
-      ]}
-      mobileBreakpoint={670}
-      showDots
-      loop
+      activeIndex={1}
+      /* next={function noRefCheck(){}}
+      previous={function noRefCheck(){}} */
     >
+      <CarouselIndicators
+        activeIndex={5}
+        items={[
+          {
+            altText: 'Slide 1',
+            caption: 'Slide 1',
+            key: 1,
+            src: 'https://picsum.photos/id/123/1200/600',
+          },
+          {
+            altText: 'Slide 2',
+            caption: 'Slide 2',
+            key: 2,
+            src: 'https://picsum.photos/id/456/1200/600',
+          },
+          {
+            altText: 'Slide 3',
+            caption: 'Slide 3',
+            key: 3,
+            src: 'https://picsum.photos/id/678/1200/600',
+          },
+        ]}
+      /* onClickHandler={function noRefCheck(){}} */
+      />
       {Slides.map((slide) => (
-        <Carousel.Item key={slide.id}>
+        <CarouselItem key={slide.id}>
           <img src={slide.url} alt='slide' width={250} height={250} />
-          <h2>{slide.text}</h2>
-        </Carousel.Item>
+          <CarouselCaption
+            captionHeader={slide.text}
+            captionText={slide.text}
+          />
+        </CarouselItem>
       ))}
+      <CarouselControl
+        direction='prev'
+        directionText='Previous'
+        /* onClickHandler={function noRefCheck(){}} */
+      />
+      <CarouselControl
+        direction='next'
+        directionText='Next'
+        /* onClickHandler={function noRefCheck(){}} */
+      />
     </Carousel>
   );
 }

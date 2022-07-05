3a8dc474b00836442b62ce0b1d5db5966ff143a1
Alicia Pak <aliciappak@gmail.com>
2022-07-05 17:19:36 -0300

    feat: some changes in styles

--- src/store/combineReducer.js
+++ src/store/combineReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import testimonials from './slicing/testimonialsSlice';
 import users from './slicing/usersSlice';
-import counter from './slicing/counterSlice';
 
 export default combineReducers({
   testimonials,
   users,
-  counter,
 });

33d9c3b982fdccc8833b37c6c8685d9429c75736
SchleicherConfalonieriN <76877741+SchleicherConfalonieriN@users.noreply.github.com>
2022-07-13 11:42:41 -0300

    Delete src directory
    delete alredy merged

--- src/components/example/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Button } from 'reactstrap';
-import styles from './example.module.css';
-
-function Example() {
-  return <Button className={styles.Button}>Hola soy un example</Button>;
-}
-
-export default Example;

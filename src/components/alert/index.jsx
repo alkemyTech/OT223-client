3c29254584d7691d7ff9a02a4c4521e35c3d8c2c
Alicia Pak <aliciappak@gmail.com>
2022-07-21 16:23:37 -0300

    close alert added

--- src/components/alert/index.jsx
+++ src/components/alert/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import SweetAlert from 'sweetalert2-react';
+import { useDispatch } from 'react-redux';
+import { closeAlert } from '../../store/slicing/controller/alertSlice';
 
 function CustomAlert({
   title ='', text = '', icon = '', state, setState,
 }) {
+  const dispatch = useDispatch();
+
   return(
     <SweetAlert
       show={state}
@@ -11,6 +15,7 @@ function CustomAlert({
       text={text}
       icon={icon}
       onConfirm={() => setState(!state)}
+      onClick={dispatch(closeAlert())}
     />
   );
 };

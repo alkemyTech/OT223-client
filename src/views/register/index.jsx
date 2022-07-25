a948f89e4ca23f524d101985ca5166b53663818e
Jeffer <jefferd2016@gmail.com>
2022-07-25 17:42:01 -0500

    fix register view

--- src/views/register/index.jsx
+++ src/views/register/index.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { Form, Button } from 'reactstrap';
+import { Form, Container, Card, CardBody, CardTitle, CardHeader } from 'reactstrap';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 /* ---- Imports to Components ---- */
 import TextForm from '../../components/TextForm';
+import CustomButton from '../../components/button';
 import validationScheme from '../../utils/validation';
 import { registerStart } from '../../store/slicing/auth/authSlice';
 import AnimationView from '../../utils/AnimationView';
@@ -39,41 +40,62 @@ function RegisterScreen() {
   }, [user])
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <TextForm
-        title='Nombre'
-        name='firstName'
-        control={control}
-        error={errors.firstName}
-      />
-      <TextForm
-        title='Apellido'
-        name='lastName'
-        control={control}
-        error={errors.lastName}
-      />
-      <TextForm
-        title='Correo Electrónico'
-        name='email'
-        control={control}
-        error={errors.email}
-      />
-      <TextForm
-        title='Contraseña'
-        name='password'
-        control={control}
-        error={errors.password}
-        type='password'
-      />
-      <TextForm
-        title='Repetir Contraseña'
-        name='repeatPassword'
-        control={control}
-        error={errors.repeatPassword}
-        type='password'
-      />
-      <Button>Submit</Button>
-    </Form>
+    <Container>
+      <Card
+        className='border-0 my-3'
+      >
+        <CardHeader
+          className='border-0 bg-transparent'
+        >
+          <CardTitle>
+            <p className='fs-2 fw-bolder'>
+              Crea una cuenta :D
+            </p>
+          </CardTitle>
+        </CardHeader>
+        <CardBody>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <TextForm
+              title='Nombre'
+              name='firstName'
+              control={control}
+              error={errors.firstName}
+            />
+            <TextForm
+              title='Apellido'
+              name='lastName'
+              control={control}
+              error={errors.lastName}
+            />
+            <TextForm
+              title='Correo Electrónico'
+              name='email'
+              control={control}
+              error={errors.email}
+            />
+            <TextForm
+              title='Contraseña'
+              name='password'
+              control={control}
+              error={errors.password}
+              type='password'
+            />
+            <TextForm
+              title='Repetir Contraseña'
+              name='repeatPassword'
+              control={control}
+              error={errors.repeatPassword}
+              type='password'
+            />
+            <CustomButton
+              variant='danger'
+              type='submit'
+              texto='Registrarse'
+            />
+          </Form>
+        </CardBody>
+      </Card>
+    </Container>
   );
 }
 

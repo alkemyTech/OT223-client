4b4899d6a80ad13f383cb525e9493f54508a4bc5
Jose Angel Cortez Mata <acortez@cmlexports.com>
2022-07-26 21:46:05 -0400

    add public, private and shared routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Route, Routes, useLocation, Outlet, Navigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 import Routers from './routers';
 
+
+const PrivateRoutes = ({ user }) => {
+  if (user && user.length > 0) return <Outlet />
+  return <Navigate to='/' />
+};
+
+const PublicRoutes = ({ user }) => {
+  if (user && user.length > 0) return <Navigate to='/' />
+  return <Outlet />
+};
+
 function RouteHandler() {
   const location = useLocation();
+  const { user } = useSelector((store) => store.auth)
+
   return (
-      <AnimatePresence>
-        <Routes location={location} key={location.pathname}>
-          {Routers.map((route) => (
-            <Route
-              path={route.path}
-              element={route.element}
-              key={route.id}
-            />
-          ))}
-        </Routes>
-      </AnimatePresence>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route>
+          {Routers.map((route) =>
+            route.type === "shared" && (
+              <Route
+                path={route.path}
+                element={route.element}
+                key={route.id}
+              />
+            ))}
+        </Route>
+        <Route element={<PublicRoutes user={user} />}>
+          {Routers.map((route) =>
+            route.type === "public" && (
+              <Route
+                path={route.path}
+                element={route.element}
+                key={route.id}
+              />
+            ))}
+        </Route>
+        <Route element={<PrivateRoutes user={user} />}>
+          {Routers.map((route) =>
+            route.type === "private" && (
+              <Route
+                path={route.path}
+                element={route.element}
+                key={route.id}
+              />
+            ))}
+        </Route>
+        <Route path='*' element={<div>NotFoundPage</div>} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
--- src/router/routers.js
+++ src/router/routers.js
@@ -13,46 +13,55 @@ const Routers = [
     path: "/",
     element: <Home />,
     id: 0,
+    type: "shared"
   },
   {
     path: "/login",
     element: <LoginScreen />,
     id: 1,
+    type: "public"
   },
   {
     path: "/registro",
     element: <RegisterScreen />,
     id: 2,
+    type: "public"
   },
   {
     path: '/inicio',
     element: <Home />,
     id: 3,
+    type: "shared"
   },
   {
     path: '/contacto',
     element: <Contact />,
     id: 4,
+    type: "shared"
   },
   {
     path: '/backoffice/*',
     element: <BackOffice />,
     id: 5,
+    type: "private"
   },
   {
     path: '/staff',
     element: <Staff />,
     id: 6,
+    type: "shared"
   },
   {
     path: '/testimonials',
     element: <Testimonials />,
     id: 7,
+    type: "shared"
   },
   {
     path: '/novedades',
     element: <News />,
     id: 8,
+    type: "shared"
   }
 ];
 

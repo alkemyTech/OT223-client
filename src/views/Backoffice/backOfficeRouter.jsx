03d89f95e06b4f38bbda7d181de1bf200995132f
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-07-26 23:46:11 -0300

    fix: fix conflicts

--- src/views/Backoffice/backOfficeRouter.jsx
+++ src/views/Backoffice/backOfficeRouter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import BackofficeForm from "../../components/BackofficeForm";
 import ListOfBackofficeCards from "../../components/ListOfBackofficeCards";
 
 const routers = [
@@ -15,7 +16,7 @@ const routers = [
   },
   {
     path: '/activities',
-    element: <h2>Actividades</h2>,
+    element: <BackofficeForm method='create' />,
     id: 2
   },
   {
